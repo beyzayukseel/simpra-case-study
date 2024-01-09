0d436cb211339722068c288a7538946cc8be2f52
beyzayukseel <68306258+beyzayukseel@users.noreply.github.com>
2024-01-10 02:05:22 +0300

    Initial commit

---
+++ src/main/java/com/beyzanur/simpracasestudy/SimpraCaseStudyApplication.java
@@ -0,0 +1,13 @@
+package com.beyzanur.simpracasestudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpraCaseStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpraCaseStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/beyzanur/simpracasestudy/SimpraCaseStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beyzanur.simpracasestudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpraCaseStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
