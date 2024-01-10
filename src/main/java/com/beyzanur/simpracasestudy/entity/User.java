b7b71b33601f508e8547f6845df4aef116711543
beyzayukseel <68306258+beyzayukseel@users.noreply.github.com>
2024-01-11 00:53:39 +0300

    fix: add UserDetails for override user details interface

--- src/main/java/com/beyzanur/simpracasestudy/entity/User.java
+++ src/main/java/com/beyzanur/simpracasestudy/entity/User.java
@@ -3,20 +3,49 @@ package com.beyzanur.simpracasestudy.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
 
 @Entity
 @Setter
 @Getter
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
+    private String username;
     private String email;
     private String password;
     @OneToOne(fetch = FetchType.EAGER)
     private Role role;
     private String created;
     private String updated;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
