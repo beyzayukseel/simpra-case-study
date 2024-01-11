297dcac64e44412f65522d21b8a51c87155772e4
beyzayukseel <68306258+beyzayukseel@users.noreply.github.com>
2024-01-12 01:50:05 +0300

    refactor: delete useless import

--- src/main/java/com/beyzanur/simpracasestudy/service/notification/MailSender.java
+++ src/main/java/com/beyzanur/simpracasestudy/service/notification/MailSender.java
@@ -2,7 +2,6 @@ package com.beyzanur.simpracasestudy.service.notification;
 
 import com.beyzanur.simpracasestudy.model.NotificationModel;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Component;
