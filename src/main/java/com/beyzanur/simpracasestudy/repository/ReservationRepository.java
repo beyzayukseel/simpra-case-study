5d3684884eed0700e458178f483662080b53b13f
beyzayukseel <68306258+beyzayukseel@users.noreply.github.com>
2024-01-11 23:39:22 +0300

    fix: fix query

--- src/main/java/com/beyzanur/simpracasestudy/repository/ReservationRepository.java
+++ src/main/java/com/beyzanur/simpracasestudy/repository/ReservationRepository.java
@@ -11,7 +11,7 @@ import java.util.List;
 @Repository
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
     @Query("SELECT r FROM Reservation r WHERE r.checkinDate = ?1 AND r.checkoutDate = ?2 AND r.rateCode.id = ?3 AND r.roomCode.id = ?4")
-    List<Reservation> findByCriteria(String checkinDate, String checkoutDate, Long rateCodeId, Long roomCodeId);
+    List<Reservation> findByCriteria(LocalDateTime checkinDate, LocalDateTime checkoutDate, Long rateCodeId, Long roomCodeId);
 
     @Query("SELECT r FROM Reservation r WHERE r.checkoutDate < ?1 AND r.checkoutDate > ?2")
     List<Reservation> getCheckedOutReservations(LocalDateTime date1, LocalDateTime date2);
